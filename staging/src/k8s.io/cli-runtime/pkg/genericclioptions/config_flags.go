3c495d99469924a800ebcd2c402bfedcf603cada
cakemanny <goldingd89@gmail.com>
2022-02-08 23:49:52 +0100

    Use redis instead of disk for kubectl cache
    Use local redis as backing for cache instead of ~/.kube/cache
    
    After applying this, need to
    
        hack/pin-dependency.sh github.com/go-redis/redis/v8 v8.11.4
        hack/update-vendor.sh
    
    in order to be able to build.
    Committing source file changes here only so that this is easier to
    cherry-pick.

--- staging/src/k8s.io/cli-runtime/pkg/genericclioptions/config_flags.go
+++ staging/src/k8s.io/cli-runtime/pkg/genericclioptions/config_flags.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/discovery"
 	diskcached "k8s.io/client-go/discovery/cached/disk"
+	rediscached "k8s.io/client-go/discovery/cached/redis"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/tools/clientcmd"
@@ -284,7 +285,12 @@ func (f *ConfigFlags) toDiscoveryClient() (discovery.CachedDiscoveryInterface, e
 	httpCacheDir := filepath.Join(cacheDir, "http")
 	discoveryCacheDir := computeDiscoverCacheDir(filepath.Join(cacheDir, "discovery"), config.Host)
 
-	return diskcached.NewCachedDiscoveryClientForConfig(config, discoveryCacheDir, httpCacheDir, time.Duration(10*time.Minute))
+	if cacheDir != defaultCacheDir {
+		return diskcached.NewCachedDiscoveryClientForConfig(config, discoveryCacheDir, httpCacheDir, time.Duration(10*time.Minute))
+	}
+	httpCacheDir = filepath.Join(cacheDir, "http")
+	discoveryCachePrefix := computeDiscoverCachePrefix(config.Host)
+	return rediscached.NewCachedDiscoveryClientForConfig(config, discoveryCachePrefix, httpCacheDir, time.Duration(10*time.Minute))
 }
 
 // ToRESTMapper returns a mapper.
@@ -456,4 +462,12 @@ func computeDiscoverCacheDir(parentDir, host string) string {
 	// now do a simple collapse of non-AZ09 characters.  Collisions are possible but unlikely.  Even if we do collide the problem is short lived
 	safeHost := overlyCautiousIllegalFileCharacters.ReplaceAllString(schemelessHost, "_")
 	return filepath.Join(parentDir, safeHost)
+}
+
+func computeDiscoverCachePrefix(host string) string {
+	// strip the optional scheme from host if its there:
+	schemelessHost := strings.Replace(strings.Replace(host, "https://", "", 1), "http://", "", 1)
+	// now do a simple collapse of non-AZ09 characters.  Collisions are possible but unlikely.  Even if we do collide the problem is short lived
+	safeHost := overlyCautiousIllegalFileCharacters.ReplaceAllString(schemelessHost, "_")
+	return safeHost
 }
